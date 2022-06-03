0ebb64df3a57a8c4dccbccf285365ea558c56c2a
Dee-pii <premrajdeepika@gmail.com>
2022-06-03 17:58:03 +0000

    I sometimes suffer from insomnia

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -16,11 +16,6 @@ for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
 {
 	putchar(alphabets);
 }
-for (alphabets = 'A'; alphabets <= 'Z'; alphabets++)
-{
-	putchar(tolower(alphabets));
-}
-
 putchar('\n');
 return (0);
 }
