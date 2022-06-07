6a3e0a6c269dc5834b9ba7a9a78b9caeeba5e4db
Dee-pii <premrajdeepika@gmail.com>
2022-06-07 23:07:27 +0000

    Task 8 - 24hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print every minute
+ * Description - a function that prints every minute of the day
+ * Return: starting from 00:00 to 23:59
+*/
+
+void jack_bauer(void)
+{
+	int b, c;
+
+	b = 0;
+	while (b < 24)
+	{
+	c = 0;
+	while (c < 60)
+	{
+		_putchar (b / 10 + '0');
+		_putchar (b % 10 + '0');
+		_putchar (':');
+		_putchar (c / 10 + '0');
+		_putchar (c % 10 + '0');
+		_putchar ('\n');
+	}
+	}
+}
